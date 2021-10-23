98ed327865b12a073f335fa8b6d99ede04a4022c
babachacha <lymickey@163.com>
2021-10-23 13:25:37 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tp3.h"
+#include "tp3.c"
+int main() {
+    char choixMenu = '0' ;
+    t_mot *tete = NULL;
+    do {
+        printf("\n========================================");
+        printf("\n  [1] Afficher un lexique");
+        printf("\n  [2] Ajouter un mot dans un lexique");
+        printf("\n  [3] Retirer un mot d'un lexique");
+        printf("\n  [4] Fusionner deux lexiques");
+        printf("\n  [5] Charger un fichier dans un lexique");
+        printf("\n  [6] Quitter");
+        printf("\n========================================");
+        printf("\nVotre choix : ");
+        choixMenu = getchar();
+        viderBuffer();
+        //t_mot *mes_mots = malloc(sizeof(t_mot));
+        switch (choixMenu) {
+            case '1' :
+                printf("\n\n\t<Faire Afficher un lexique>\n\n");
+                afficherMots(tete);
+                break;
+            case '2' :
+                printf("\n\n\t<Faire Ajouter un mot dans un lexique>\n\n");
+                printf("\n\n\t<Entrez le mot:>\n\n");
+                char nouveau_mot[20]="\0";
+                gets(nouveau_mot);
+                tete = ajouterMot(tete,nouveau_mot);
+                afficherMots(tete);
+                break;
+            case '3' :
+                printf("\n\n\t<Faire Retirer un mot d'un lexique>\n\n");
+                break;
+            case '4' :
+                printf("\n\n\t<Faire Fusionner deux lexiques>\n\n");
+                break;
+            case '5' :
+                printf("\n\n\t<Faire Charger un fichier dans un lexique>\n\n");
+                break;
+        }
+    } while (choixMenu != '6');
+
+
+    printf("\n\n*** FIN DU PROGRAMME ***\n");
+
+    return 0;
+}
---
+++ tp3.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "tp3.h"
+
+// Création nouvel élément
+t_mot *creerMot(char *mot) {
+    t_mot *nouveau = malloc(sizeof(t_mot));
+    if(nouveau != NULL){
+        strcpy(nouveau->mot,mot);
+        nouveau->nombre_occurences = 1;
+        nouveau->suivant = NULL;
+    }
+    return nouveau;
+}
+/* ====== FIN creerMot ====== */
+
+// Ajout mot dans une liste
+t_mot *ajouterMot(t_mot *liste, char *mot) {
+    if(mot == NULL)return liste;
+    t_mot *precedent = NULL,*courant = liste;
+    for(int i=0; i<strlen(mot);i++){mot[i] = tolower(mot[i]);}
+    if(liste == NULL || strcmp(liste->mot,mot) > 0){
+        t_mot *nouveau = creerMot(mot);
+        nouveau->suivant = liste;
+        return nouveau;
+    }
+    while(courant != NULL && strcmp(courant->mot,mot) < 0){
+        precedent = courant;
+        courant = courant->suivant;
+    }
+
+    if(courant == NULL || strcmp(courant->mot,mot) > 0){
+        t_mot *nouveau = creerMot(mot);
+        precedent->suivant = nouveau;
+        nouveau->suivant = courant;
+    }else{
+        courant->nombre_occurences++;
+    }
+    return liste;
+}
+/* ====== FIN  ====== */
+
+
+// Retrait occurence mot d'une liste
+t_mot *retirerMot(t_mot *liste, char *mot) {
+    return NULL; // à remplacer par le code la fonctino
+}
+/* ====== FIN ajouterMot ====== */
+
+
+// Affichage mots d'un lexique
+void afficherMots(t_mot *liste){
+    t_mot *courant = liste;
+    if(liste == NULL){
+        printf("\n\n\tLA LISTE EST VIDE\n\n");
+        return;
+    }
+    char prefixe = '-';
+    printf("\n\n==================");
+    while(courant != NULL){
+        if(courant->mot[0] == prefixe){
+            printf("\n    --- %s [%d]",courant->mot,courant->nombre_occurences);
+        }else{
+            prefixe = courant->mot[0];
+            printf("\n%c   --- %s [%d]",toupper(prefixe),courant->mot,courant->nombre_occurences);
+        }
+        courant = courant->suivant;
+    }
+    printf("\n==================\n\n");
+}
+/* ====== FIN afficherMots ====== */
+
+
+// Fusion listes
+t_mot *fusionner(t_mot *listeA, t_mot *listeB){
+    return NULL; // à remplacer par le code la fonctino
+}
+/* ====== FIN fusionner ====== */
+
+
+// Import fichier de mots dans une liste
+t_mot *importerFichier(t_mot *liste){
+    return NULL; // à remplacer par le code la fonctino
+}
+/* ====== FIN importerFichier ====== */
+
+
+
+
+// fonction utile pour vider le tampon clavier
+void viderBuffer() {
+    int c = '0';
+    while (c!='\n' && c != EOF) { c = getchar(); }
+}
